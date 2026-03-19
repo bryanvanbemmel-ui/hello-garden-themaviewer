ee2cd138b82b21a744f94e6330032574d9413c1f
bryanvanbemmel-ui <bryanvanbemmel@gmail.com>
2026-03-19 15:54:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -60,20 +60,3 @@ function render(list) {
     `;
   }).join("");
 }
-#suggestions {
-  max-width: 700px;
-  background: white;
-  border: 1px solid #ddd;
-  border-radius: 10px;
-  margin-bottom: 10px;
-}
-
-.suggestion {
-  padding: 10px;
-  cursor: pointer;
-  border-bottom: 1px solid #eee;
-}
-
-.suggestion:hover {
-  background: #f3f4f6;
-}
